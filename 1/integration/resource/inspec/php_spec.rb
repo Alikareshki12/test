0a2946ccaedb2f9910c6dd7ef71f4b97d0842947
Ali <alikareshki99@gmail.com>
2021-02-25 18:58:46 +0100

    =1

---
+++ 1/integration/resource/inspec/php_spec.rb
@@ -0,0 +1,30 @@
+describe 'PHP' do
+  it 'has php' do
+    expect(command('php -v').exit_status).to eq(0)
+  end
+
+  it 'has the pear.php.net channel' do
+    expect(command('pear list-channels').stdout).to include('pear.php.net')
+  end
+
+  it 'has the pecl.php.net channel' do
+    expect(command('pear list-channels').stdout).to include('pecl.php.net')
+  end
+
+  it 'has the PECL sync module' do
+    expect(command('php --ri sync').exit_status).to eq(0)
+  end
+
+  unless os[:family] == 'redhat'
+    it 'has the correct priority set' do
+      expect(command('php -i').stdout).to include('50-sync')
+    end
+  end
+end
+
+# Check if we didn't accidentally pull in Apache as a dependency (#311)
+unless os[:family] == 'redhat'
+  describe package('apache2') do
+    it { should_not be_installed }
+  end
+end
